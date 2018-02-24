9285f12fe1b3e850b08d7fcdae92cf618d95c2fb
manoj062006 <manoj062006@hotmail.com>
2018-02-24 10:08:05 +0530

    Add project files.

---
+++ assignment_1_q_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment_1_q_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
